6089438235c29cf07bfbf0ed367a53de5e84bbaf
Bruno Emílio <36413641+BrnEmilio@users.noreply.github.com>
2025-06-22 09:31:14 -0300

    Update test_pipeline.py

--- test_pipeline.py
+++ test_pipeline.py
@@ -6,9 +6,3 @@ from sklearn.metrics import accuracy_score
 def test_model_files_exist():
     assert os.path.exists("./model.joblib"), "Modelo não encontrado"
     assert os.path.exists("./vectorizer.joblib"), "Vectorizer não encontrado"
-
-def test_vectorizer_output_shape():
-    vectorizer = joblib.load("./vectorizer.joblib")
-    sample = ["Este é um ótimo produto!"]
-    vetor = vectorizer.transform(sample)
-    assert vetor.shape[0] == 1, "Vectorizer retornou forma incorreta"
